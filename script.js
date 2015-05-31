e566df33c4708238041d4ec3644480f919d1ef10
Julien Duponchelle <julien@duponchelle.info>
2015-05-31 20:00:12 +0200

    More stats

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var repositories = {
-    "docker/docker": {"raise": 150000000, "name": "Docker"},
-    "PrestaShop/PrestaShop": {"raise": 14800000, "name": "PrestaShop"}
+    "docker/docker": {"raise": 150, "name": "Docker"},
+    "basho/riak": {"raise": 57.5 ,"name": "Riak"},
+    "mongodb/mongo": {"raise": 311.1 ,"name": "MongoDB"},
+    "elastic/elasticsearch": {"raise": 104, "name": "Elastic Search"},
+    "PrestaShop/PrestaShop": {"raise": 14.8, "name": "PrestaShop"}
 };
 
 $(function() {
@@ -21,6 +24,20 @@ function getRepo(repository) {
     return promise;
 }
 
+function addResultRow(raise, reference, repo, key, name) {
+    var tr = $("<tr>");
+
+    tr.append($("<td>").text(name));
+    tr.append($("<td>").text(reference[key]));
+    tr.append($("<td>").text(repo[key]));
+
+    var valuation = Math.round(raise / reference[key] * repo[key]);
+    var one = Math.round(raise / reference[key]);
+    tr.append($("<td>").text(one.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " $ / " + name));
+    tr.append($("<td>").text(valuation.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " $"));
+    return tr;
+}
+
 function onSubmit() {
     var repo = $('input[name="repository"]').val();
     var reference = $('select[name="reference"]').val();
@@ -30,11 +47,30 @@ function onSubmit() {
         var stats = $("#stats");
         console.log(repo);
         stats.html("");
+
+        var raise = repositories[reference["full_name"]]["raise"] * 1000000;
+
         stats.append($("<h1>").text(reference["name"] + " / " + repo["name"]));
-        stats.append($("<p>").html(reference["watchers_count"] + " &#x2605; / " + repo["watchers_count"] + " &#x2605;"));
-        var raise = repositories[reference["full_name"]]["raise"];
-        var valuation = Math.round(raise / reference["watchers_count"] * repo["watchers_count"]);
-        stats.append($("<p>").text(valuation.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " $ of fund raising"));
+        stats.append($("<p>")
+            .text(reference["name"] + " raised " + raise.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " $")
+            .attr('id', 'raise'));
+
+        var table = $("<table>");
+
+        var tr = $("<tr>");
+        tr.append($("<th>"));
+        tr.append($("<th>").text(reference["name"]));
+        tr.append($("<th>").text(repo["name"]));
+        tr.append($("<th>").text("Value for one element"));
+        tr.append($("<th>").text("Estimate fund raising"));
+        table.append(tr);
+
+        table.append(addResultRow(raise, reference, repo, "stargazers_count", "stargazers"));
+        table.append(addResultRow(raise, reference, repo, "subscribers_count", "subscribers"));
+        table.append(addResultRow(raise, reference, repo, "forks", "forks"));
+        table.append(addResultRow(raise, reference, repo, "open_issues", "open issues"));
+        stats.append(table);
+
         //TODO: Display watcher price
     });
 
